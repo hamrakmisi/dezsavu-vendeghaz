fd1f7e46073e8a69fff6659da7da88fb50a44b9d
hamrakmisi <misi@hamy.hu>
2025-09-21 10:48:12 +0200

    Adding storybook to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,32 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./.storybook/preview.js",
+    "./src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'var(--primary)',
+          foreground: 'var(--primary-foreground)',
+        },
+      },
+    },
   },
-  plugins: [require("rippleui")],
+  plugins: [
+    require("rippleui")({
+      themes: [
+        {
+          themeName: "light",
+          colorScheme: "light",
+        },
+        {
+          themeName: "dark",
+          colorScheme: "dark",
+        },
+      ],
+    }),
+  ],
 };
