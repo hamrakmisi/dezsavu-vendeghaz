078ff38021b14a1d90f4a5f7aea1a4bd3fd0a3e4
Mihály Hamrák <mihalyhamrak@gmail.com>
2026-01-30 11:45:03 +0100

    rmove unused import

--- src/app/admin/components/LoginForm.tsx
+++ src/app/admin/components/LoginForm.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, { useState } from 'react'
-import Button from '@/components/Button'
 import { useRouter } from 'next/navigation'
 
 export default function LoginForm() {
